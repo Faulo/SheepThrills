d184b68c919fdfc5f7826a5e8da70efe7e26a98a
Daniel Schulz <faulolio@gmail.com>
2021-11-06 11:39:54 +0100

    Add sheep camera

--- Franken Game Jam 21/Assets/Scripts/Player/Herd.cs
+++ Franken Game Jam 21/Assets/Scripts/Player/Herd.cs
@@ -1,19 +1,33 @@
 using System.Collections.Generic;
+using Cinemachine;
 using Slothsoft.UnityExtensions;
 using UnityEngine;
 
 namespace TheSheepGame.Player {
     public class Herd : MonoBehaviour {
+        [Header("MonoBehaviour configuration")]
+        [SerializeField, Expandable]
+        public CinemachineVirtualCamera virtualCamera = default;
+        [SerializeField, Expandable]
+        public CinemachineTargetGroup cameraGroup = default;
+
+        [Header("Herd configuration")]
         [SerializeField]
-        public Vector3 direction = Vector3.forward;
+        float maxSpeed = 5;
         [SerializeField]
-        public float speed = 5;
+        AnimationCurve speedOverCount = AnimationCurve.Constant(0, 1, 1);
         [SerializeField]
         float food = 0;
         [SerializeField]
         float foodPerSheep = 10;
         [SerializeField]
         int maxSheepCount = 100;
+
+        [Header("Debug fields")]
+        [SerializeField]
+        public float speed = 0;
+        [SerializeField]
+        public Vector3 direction = Vector3.forward;
 
         public readonly List<Sheep> sheepList = new List<Sheep>();
         public int sheepCount => sheepList.Count;
@@ -25,7 +39,7 @@ namespace TheSheepGame.Player {
         }
 
         protected void FixedUpdate() {
-            food += sheepList.Count * Time.deltaTime;
+            food += Time.deltaTime;
             if (sheepList.Count < maxSheepCount && food > foodPerSheep) {
                 food -= foodPerSheep;
                 Multiply();
@@ -40,6 +54,8 @@ namespace TheSheepGame.Player {
         void AddSheep(Sheep sheep) {
             sheep.herd = this;
             sheepList.Add(sheep);
+            cameraGroup.AddMember(sheep.transform, 1, 1);
+            speed = maxSpeed * speedOverCount.Evaluate((float)sheepList.Count / maxSheepCount);
         }
     }
 }
