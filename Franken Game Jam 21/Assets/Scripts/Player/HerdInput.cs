1fe07635c206c04bec907619675c0a116f190a04
Nic <nicolas@raveyard.de>
2021-11-06 12:18:49 +0100

    Added input to destructibles

--- Franken Game Jam 21/Assets/Scripts/Player/HerdInput.cs
+++ Franken Game Jam 21/Assets/Scripts/Player/HerdInput.cs
@@ -4,31 +4,35 @@ using UnityEngine.InputSystem;
 
 namespace TheSheepGame.Player {
     public class HerdInput : MonoBehaviour {
-        [SerializeField]
-        Herd herd = default;
-        [SerializeField]
-        InputActionAsset actionsAsset = default;
+        [SerializeField] Herd herd = default;
+        [SerializeField] InputActionAsset actionsAsset = default;
 
         InputActionAsset actionsInstance;
 
         protected void Awake() {
             OnValidate();
         }
+
         protected void OnValidate() {
             if (!herd) {
                 TryGetComponent(out herd);
             }
         }
+
         protected void OnEnable() {
             actionsInstance = Instantiate(actionsAsset);
+            actionsAsset["Destroy"].performed += OnDestroyPressed;
             actionsInstance.Enable();
         }
+
         protected void OnDisable() {
             if (actionsInstance) {
                 actionsInstance.Disable();
+                actionsAsset["Destroy"].performed -= OnDestroyPressed;
                 Destroy(actionsInstance);
             }
         }
+
         protected void Update() {
             var move = actionsInstance["Move"].ReadValue<Vector2>();
             if (move != Vector2.zero) {
@@ -38,6 +42,14 @@ namespace TheSheepGame.Player {
                 herd.Bite();
             }
         }
+
+        private void OnDestroyPressed(InputAction.CallbackContext context) {
+            if (!context.performed) {
+                return;
+            }
+            //TODO: Herd.Bite()
+        }
+
         protected void OnDrawGizmos() {
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(transform.position, 1);
