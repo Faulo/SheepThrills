7d22b8ee49db097c35804a335ba5fc43f3a79c47
Daniel Schulz <faulolio@gmail.com>
2021-11-06 22:41:41 +0100

    Add SFX

--- Franken Game Jam 21/Assets/Scripts/OneShotAudio.cs
+++ Franken Game Jam 21/Assets/Scripts/OneShotAudio.cs
@@ -1,16 +1,24 @@
+using System;
+using Slothsoft.UnityExtensions;
 using UnityEngine;
 
 namespace TheSheepGame {
     public class OneShotAudio : MonoBehaviour {
         [SerializeField]
-        AudioSource audioSource = default;
+        AudioClip[] clips = Array.Empty<AudioClip>();
+        [SerializeField, Range(0, 10)]
+        float pitchMininum = 1;
+        [SerializeField, Range(0, 10)]
+        float pitchMaximum = 1;
+
+        AudioSource audioSource;
         protected void Awake() {
-            OnValidate();
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.pitch = UnityEngine.Random.Range(pitchMininum, pitchMaximum);
+            audioSource.clip = clips.RandomElement();
         }
-        protected void OnValidate() {
-            if (!audioSource) {
-                TryGetComponent(out audioSource);
-            }
+        protected void Start() {
+            audioSource.Play();
         }
         protected void Update() {
             if (!audioSource.isPlaying) {
