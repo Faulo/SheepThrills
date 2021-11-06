cce7e6913abdef8d71c3528eaeceaeb5a52d20fe
Daniel Schulz <faulolio@gmail.com>
2021-11-06 22:54:01 +0100

    Mix audio

--- Franken Game Jam 21/Assets/Scripts/OneShotAudio.cs
+++ Franken Game Jam 21/Assets/Scripts/OneShotAudio.cs
@@ -1,11 +1,14 @@
 using System;
 using Slothsoft.UnityExtensions;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace TheSheepGame {
     public class OneShotAudio : MonoBehaviour {
         [SerializeField]
         AudioClip[] clips = Array.Empty<AudioClip>();
+        [SerializeField, Expandable]
+        AudioMixerGroup mixer = default;
         [SerializeField, Range(0, 10)]
         float pitchMininum = 1;
         [SerializeField, Range(0, 10)]
@@ -16,6 +19,7 @@ namespace TheSheepGame {
             audioSource = gameObject.AddComponent<AudioSource>();
             audioSource.pitch = UnityEngine.Random.Range(pitchMininum, pitchMaximum);
             audioSource.clip = clips.RandomElement();
+            audioSource.outputAudioMixerGroup = mixer;
         }
         protected void Start() {
             audioSource.Play();
