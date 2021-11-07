427082a0498c993bf410af30feadc67b24af1cdb
Daniel Schulz <faulolio@gmail.com>
2021-11-07 15:13:39 +0100

    Update main menu

--- Franken Game Jam 21/Assets/Scripts/GameManager.cs
+++ Franken Game Jam 21/Assets/Scripts/GameManager.cs
@@ -1,49 +1,20 @@
 using MyBox;
-using TheSheepGame.Player;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace TheSheepGame {
     public class GameManager : Singleton<GameManager> {
-        [SerializeField]
-        InputActionAsset actionsAsset = default;
-
-        InputActionAsset actionsInstance;
-
-        protected void OnEnable() {
-            actionsInstance = Instantiate(actionsAsset);
-            actionsInstance[nameof(Menu)].performed += Menu;
-            actionsInstance.Enable();
-        }
-        protected void OnDisable() {
-            if (actionsInstance) {
-                actionsInstance.Disable();
-                actionsInstance[nameof(Menu)].performed -= Menu;
-                Destroy(actionsInstance);
-            }
-        }
-        void Menu(InputAction.CallbackContext context) {
-            var herd = FindObjectOfType<Herd>();
-            if (herd) {
-                herd.Lose();
-            } else {
-                Quit();
-            }
+        public void LoadScene(string name) {
+            SceneManager.LoadScene(name);
         }
         public void Restart() {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         public void Quit() {
-            var herd = FindObjectOfType<Herd>();
-            if (herd) {
-                herd.Lose();
-            } else {
-                Application.Quit();
+            Application.Quit();
 #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #endif
-            }
         }
     }
 }
