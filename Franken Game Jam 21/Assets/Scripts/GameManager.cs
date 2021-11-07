a82293ffb8c51659ebfb54007738ff79fc7425a1
Daniel Schulz <faulolio@gmail.com>
2021-11-07 15:03:25 +0100

    Add game over state

--- Franken Game Jam 21/Assets/Scripts/GameManager.cs
+++ Franken Game Jam 21/Assets/Scripts/GameManager.cs
@@ -1,27 +1,49 @@
-using System.Collections;
 using MyBox;
-using Slothsoft.UnityExtensions;
 using TheSheepGame.Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace TheSheepGame {
     public class GameManager : Singleton<GameManager> {
-        [SerializeField, Range(0, 10)]
-        float restartTimeout = 1;
+        [SerializeField]
+        InputActionAsset actionsAsset = default;
+
+        InputActionAsset actionsInstance;
 
         protected void OnEnable() {
-            Herd.onLose += HandleLose;
+            actionsInstance = Instantiate(actionsAsset);
+            actionsInstance[nameof(Menu)].performed += Menu;
+            actionsInstance.Enable();
         }
         protected void OnDisable() {
-            Herd.onLose -= HandleLose;
+            if (actionsInstance) {
+                actionsInstance.Disable();
+                actionsInstance[nameof(Menu)].performed -= Menu;
+                Destroy(actionsInstance);
+            }
         }
-        void HandleLose() {
-            StartCoroutine(RestartRoutine());
+        void Menu(InputAction.CallbackContext context) {
+            var herd = FindObjectOfType<Herd>();
+            if (herd) {
+                herd.Lose();
+            } else {
+                Quit();
+            }
         }
-        IEnumerator RestartRoutine() {
-            yield return Wait.forSeconds[restartTimeout];
+        public void Restart() {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        public void Quit() {
+            var herd = FindObjectOfType<Herd>();
+            if (herd) {
+                herd.Lose();
+            } else {
+                Application.Quit();
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            }
         }
     }
 }
